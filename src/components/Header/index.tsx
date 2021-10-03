886c9c526d31e90746fe9209c8d829505da83583
FrisonJr <jair.frison@gmail.com>
2021-10-03 19:25:26 -0300

    header and pages fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,13 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import styles from './styles.module.scss';
 
-export default function Header() {
+
+export function Header() {
+const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR,
+});
     return (
         <header className={styles.headerContainer}>
             <img src="/logo.svg" alt="Podcast" />
@@ -10,7 +17,7 @@ export default function Header() {
             </p>
 
             <span>
-                Qui, 8 Abril
+               {currentDate}
             </span>
         </header>
     );

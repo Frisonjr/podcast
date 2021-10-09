42c4584ff6859418f411827d0716df48fa703f21
Frisonjr <jair.frison@gmail.com>
2021-10-09 03:47:16 -0300

    finalizacao das paginas e format no codigo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,20 @@
-import format from 'date-fns/format';
-import ptBR from 'date-fns/locale/pt-BR';
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
 
-import styles from './styles.module.scss';
-
+import styles from "./styles.module.scss";
 
 export function Header() {
-const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
     locale: ptBR,
-});
-    return (
-        <header className={styles.headerContainer}>
-            <img src="/logo.svg" alt="Podcast" />
+  });
 
-            <p>
-                O melhor para você ouvir sempre!
-            </p>
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr" />
 
-            <span>
-               {currentDate}
-            </span>
-        </header>
-    );
+      <p>O melhor para você ouvir, sempre</p>
+
+      <span>{currentDate}</span>
+    </header>
+  );
 }
